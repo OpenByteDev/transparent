ea0071797985a9302fb43a85a1a1a31e6486f3c5
OpenByte <development.openbyte@gmail.com>
2021-10-19 23:06:28 +0200

    Make windows imports specific

--- virtual-desktop-runner/build.rs
+++ virtual-desktop-runner/build.rs
@@ -1,10 +1,41 @@
 fn main() {
     windows::build! {
-        Windows::Win32::System::Threading::*,
-        Windows::Win32::System::StationsAndDesktops::*,
-        Windows::Win32::System::Pipes::CreatePipe,
-        Windows::Win32::Foundation::*,
-        Windows::Win32::UI::WindowsAndMessaging::DESKTOP_CREATEWINDOW,
-        Windows::Win32::System::WindowsProgramming::INFINITE,
+        Windows::Win32::{
+            Security::SECURITY_ATTRIBUTES,
+            UI::{
+                DisplayDevices::DEVMODEW,
+                WindowsAndMessaging::DESKTOP_CREATEWINDOW
+            },
+            System::{
+                Threading::{
+                    PROCESS_CREATION_FLAGS,
+                    // WAIT_OBJECT_0,
+                    WAIT_RETURN_CAUSE,
+                    STARTUPINFOW,
+                    // STARTF_USESTDHANDLES,
+                    CreateProcessW,
+                    PROCESS_INFORMATION,
+                    PROCESS_CREATION_FLAGS,
+                    CreateEventW,
+                    SetEvent,
+                    WaitForMultipleObjects,
+                    GetExitCodeProcess,
+                    TerminateProcess
+                },
+                WindowsProgramming::INFINITE,
+                Pipes::CreatePipe,
+                StationsAndDesktops::{
+                    HDESK,
+                    CreateDesktopW,
+                    CloseDesktop,
+                }
+            },
+        },
+        Windows::Win32::Foundation::{
+            NTSTATUS,
+            CloseHandle,
+            SetHandleInformation,
+            STATUS_PENDING
+        },
     };
 }
