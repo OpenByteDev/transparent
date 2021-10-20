02112ff7256df6e231769b1aa24c03ae257834a3
OpenByte <development.openbyte@gmail.com>
2021-10-20 20:04:46 +0200

    Add documentation

--- README.md
+++ README.md
@@ -6,7 +6,27 @@
 [![dependency status](https://deps.rs/repo/github/openbytedev/transparent/status.svg)](https://deps.rs/repo/github/openbytedev/transparent)
 [![MIT](https://img.shields.io/crates/l/transparent.svg)](https://github.com/OpenByteDev/transparent/blob/master/LICENSE)
 
-A crate for running processes on a virtual desktop.
+A crate for running processes on a virtual desktop / X server environment.
+
+## Usage
+This will spawn `some program` on a new virtual desktop / X server environment.
+```rs
+Command::new("some program")
+    .spawn_transparent(&TransparentRunner::new())
+    .unwrap()
+    .wait()
+    .unwrap();
+```
+
+## How it works
+### Windows
+On windows `transparent` uses [`CreateProcessW`](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-createdesktopw) to create a new desktop and then spawns a child process using [`CreateProcessW`](https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-createprocessw) with [`lpStartupInfo.lpDesktop`](https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/ns-processthreadsapi-startupinfow#syntax) set to the new desktop. (Actually a helper process is spawned which then in turn spawns the target process; see [`virtual-desktop-runner`](https://github.com/OpenByteDev/transparent/tree/master/virtual-desktop-runner)).
+
+### Unix
+On unix `transparent` uses [`xvfb-run`](http://manpages.ubuntu.com/manpages/trusty/man1/xvfb-run.1.html) which runs the target application in a virtual X server environment.
+
+## Known issues
+It is currently impossible to determine the specified [`Stdio`](https://doc.rust-lang.org/std/process/struct.Stdio.html) of a [`Command`](https://doc.rust-lang.org/std/process/struct.Command.html) without using [`mem::transmute`](https://doc.rust-lang.org/std/mem/fn.transmute.html) or similar, which is why `transparent` always uses [`Stdio::piped()`](https://doc.rust-lang.org/std/process/struct.Stdio.html#method.piped).
 
 ## License
 Licensed under the MIT license ([LICENSE](https://github.com/OpenByteDev/transparent/blob/master/LICENSE) or http://opensource.org/licenses/MIT)
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,30 @@
 #![feature(once_cell)]
+/*!
+A crate for running processes on a virtual desktop / virtual X server environment.
+
+## Usage
+This will spawn `some program` on a virtual desktop / virtual X server environment.
+```rs
+Command::new("some program")
+    .spawn_transparent(&TransparentRunner::new())
+    .unwrap()
+    .wait()
+    .unwrap();
+```
+
+## How it works
+### Windows
+On windows `transparent` uses [`CreateProcessW`](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-createdesktopw) to create a new desktop and then spawns a child process using [`CreateProcessW`](https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-createprocessw) with [`lpStartupInfo.lpDesktop`](https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/ns-processthreadsapi-startupinfow#syntax) set to the new desktop. (Actually a helper process is spawned which then in turn spawns the target process; see [`virtual-desktop-runner`](https://github.com/OpenByteDev/transparent/tree/master/virtual-desktop-runner)).
+
+### Unix
+On unix `transparent` uses [`xvfb-run`](http://manpages.ubuntu.com/manpages/trusty/man1/xvfb-run.1.html) which runs the target application in a virtual X server environment.
+
+## Known issues
+It is currently impossible to determine the specified [`Stdio`](https://doc.rust-lang.org/std/process/struct.Stdio.html) of a [`Command`](https://doc.rust-lang.org/std/process/struct.Command.html) without using [`mem::transmute`](https://doc.rust-lang.org/std/mem/fn.transmute.html) or similar, which is why `transparent` always uses [`Stdio::piped()`](https://doc.rust-lang.org/std/process/struct.Stdio.html#method.piped).
+
+## License
+Licensed under the MIT license ([LICENSE](https://github.com/OpenByteDev/transparent/blob/master/LICENSE) or <http://opensource.org/licenses/MIT>)
+!*/
 
 #[cfg(windows)]
 mod windows;
@@ -17,14 +43,19 @@ mod unix;
 #[cfg(unix)]
 use unix as platform;
 
+/// Platform-dependent state required to run processes transparently.
 #[derive(Clone, Debug, Default)]
 pub struct TransparentRunner(platform::TransparentRunnerImpl);
 
 impl TransparentRunner {
+    /// Creates a new [`TransparentRunner`].
     pub fn new() -> Self {
         Self(platform::TransparentRunnerImpl::default())
     }
 
+    /// Spawns the given [`Command`] transparently:
+    ///  - on windows it is spawned on a new virtual desktop
+    ///  - on unix it is spawned in a virtual X server environment
     pub fn spawn_transparent(&self, command: &Command) -> io::Result<TransparentChild> {
         self.0
             .spawn_transparent(command)
@@ -32,10 +63,12 @@ impl TransparentRunner {
     }
 }
 
+/// Representation of a running or exited child process that was spawned using [`TransparentRunner::spawn_transparent`].
 #[derive(Debug)]
 pub struct TransparentChild(Child, TransparentRunner);
 
 impl TransparentChild {
+    /// Gets the [`TransparentRunner`] used to run the source [`Command`].
     pub fn runner(&self) -> &TransparentRunner {
         &self.1
     }
@@ -85,6 +118,9 @@ pub trait CommandExt {
 }
 
 impl CommandExt for Command {
+    /// Spawns the given [`Command`] transparently:
+    ///  - on windows it is spawned on a new virtual desktop
+    ///  - on unix it is spawned in a virtual X server environment
     fn spawn_transparent(&self, runner: &TransparentRunner) -> io::Result<TransparentChild> {
         runner.spawn_transparent(self)
     }
