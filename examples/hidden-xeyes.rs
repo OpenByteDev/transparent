511fe0b007eac04e9f99b1e2ef36e530a2db8a94
OpenByte <development.openbyte@gmail.com>
2021-10-20 19:12:47 +0200

    Check exit status in xeyes example

--- examples/hidden-xeyes.rs
+++ examples/hidden-xeyes.rs
@@ -1,7 +1,9 @@
+#![feature(exit_status_error)]
+
 use std::process::Command;
 
 use transparent::{CommandExt, TransparentRunner};
 
 fn main() {
-    Command::new("xeyes").spawn_transparent(&TransparentRunner::new()).unwrap().wait().unwrap();
+    Command::new("xeyes").spawn_transparent(&TransparentRunner::new()).unwrap().wait().unwrap().exit_ok().unwrap();
 }
