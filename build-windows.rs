6a786c635ae7231316a5edfd2d21958119a11fdb
OpenByte <development.openbyte@gmail.com>
2021-10-20 19:04:43 +0200

    Do not copy helper crate to temp dir of Cargo.toml is present

--- build-windows.rs
+++ build-windows.rs
@@ -20,59 +20,64 @@ pub fn main() {
         .to_path_buf();
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     let source_runner_crate_path = current_dir.join(runner_crate_name);
-    let target_runner_crate_dir = tempfile::Builder::new()
-        .prefix("virtual-desktop-runner-")
-        .tempdir()
-        .unwrap();
-    let target_runner_crate_path = target_runner_crate_dir.path();
-    let target_runner_crate_real_config = target_runner_crate_path.join("Cargo.toml");
-    let target_runner_crate_fake_config =
-        target_runner_crate_path.join("Cargo.toml.why-no-bin-deps");
+    let source_runner_crate_real_config = source_runner_crate_path.join("Cargo.toml");
 
     cargo_emit::rerun_if_changed!("{}", source_runner_crate_path.display());
 
-    let gitignore_path = source_runner_crate_path.join(".gitignore");
-    let gitignore_file = gitignore::File::new(&gitignore_path).ok();
-    let gitignore_filter = move |path: &Path| {
-        !gitignore_file
-            .as_ref()
-            .map(|f| f.is_excluded(path).unwrap())
-            .unwrap_or(false)
+    let target_runner_crate_dir: Box<dyn AsRef<Path>> = if source_runner_crate_real_config.exists() {
+        Box::new(source_runner_crate_path)
+    } else {
+        let target_runner_crate_dir = tempfile::Builder::new()
+            .prefix("virtual-desktop-runner-")
+            .tempdir()
+            .unwrap();
+        let target_runner_crate_path = target_runner_crate_dir.path();
+
+        let gitignore_path = source_runner_crate_path.join(".gitignore");
+        let gitignore_file = gitignore::File::new(&gitignore_path).ok();
+        let gitignore_filter = move |path: &Path| {
+            !gitignore_file
+                .as_ref()
+                .map(|f| f.is_excluded(path).unwrap())
+                .unwrap_or(false)
+        };
+
+        for entry in WalkDir::new(&source_runner_crate_path)
+            .into_iter()
+            .filter_map(|e| e.ok())
+            .filter(|e| gitignore_filter(e.path()))
+        {
+            if entry.path_is_symlink() {
+                continue;
+            }
+
+            let source_path = entry.path();
+            let relative_path = source_path.strip_prefix(&source_runner_crate_path).unwrap();
+            let target_path = target_runner_crate_path.join(relative_path);
+            if entry.file_type().is_dir() {
+                fs::create_dir(&target_path).unwrap();
+            } else {
+                assert!(entry.file_type().is_file());
+                fs::copy(&source_path, &target_path).unwrap();
+            }
+        }
+        drop(gitignore_filter);
+
+        fs::copy(
+            target_runner_crate_path.join("Cargo.toml.why-no-bin-deps"),
+            target_runner_crate_path.join("Cargo.toml"),
+        )
+        .unwrap();
+
+        Box::new(target_runner_crate_dir)
     };
-
-    for entry in WalkDir::new(&source_runner_crate_path)
-        .into_iter()
-        .filter_map(|e| e.ok())
-        .filter(|e| gitignore_filter(e.path()))
-    {
-        if entry.path_is_symlink() {
-            continue;
-        }
-
-        let source_path = entry.path();
-        let relative_path = source_path.strip_prefix(&source_runner_crate_path).unwrap();
-        let target_path = target_runner_crate_path.join(relative_path);
-        if entry.file_type().is_dir() {
-            fs::create_dir(&target_path).unwrap();
-        } else {
-            assert!(entry.file_type().is_file());
-            fs::copy(&source_path, &target_path).unwrap();
-        }
-    }
-    drop(gitignore_filter);
-
-    fs::copy(
-        &target_runner_crate_fake_config,
-        &target_runner_crate_real_config,
-    )
-    .unwrap();
 
     let mut command = Command::new("cargo");
     command
         .arg("build")
         .arg("--target")
         .arg(&target_triple)
-        .current_dir(&target_runner_crate_dir);
+        .current_dir(target_runner_crate_dir.as_ref());
     if profile == Profile::Release {
         command.arg("--release");
     }
@@ -80,7 +85,8 @@ pub fn main() {
     command.spawn().unwrap().wait().unwrap().exit_ok().unwrap();
 
     let runner_executable_filename = format!("{}.exe", runner_crate_name);
-    let runner_executable_path = target_runner_crate_path
+    let runner_executable_path = target_runner_crate_dir.as_ref()
+        .as_ref()
         .join("target")
         .join(&target_triple)
         .join(profile.as_str())
