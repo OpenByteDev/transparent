0690b8afe34d2b5417c2b8126959ab629fad97b6
OpenByte <development.openbyte@gmail.com>
2021-10-20 03:01:21 +0200

    Finaly rework of helper crate build?

--- build-windows.rs
+++ build-windows.rs
@@ -9,21 +9,22 @@ pub fn main() {
     let target_triple = build_target::target_triple().unwrap();
 
     let runner_crate_name = "virtual-desktop-runner";
-    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     let current_dir = env::current_dir()
         .unwrap()
         .absolutize()
         .unwrap()
         .to_path_buf();
-    let source_runner_crate_dir = current_dir.join(runner_crate_name);
-    let target_runner_crate_dir = out_dir.join(runner_crate_name);
-    let target_runner_crate_real_config = target_runner_crate_dir.join("Cargo.toml");
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let source_runner_crate_path = current_dir.join(runner_crate_name);
+    let target_runner_crate_dir = tempfile::TempDir::new().unwrap();
+    let target_runner_crate_path = target_runner_crate_dir.path();
+    let target_runner_crate_real_config = target_runner_crate_path.join("Cargo.toml");
     let target_runner_crate_fake_config =
-        target_runner_crate_dir.join("Cargo.toml.why-no-bin-deps");
+        target_runner_crate_path.join("Cargo.toml.why-no-bin-deps");
 
-    cargo_emit::rerun_if_changed!("{}", source_runner_crate_dir.display());
+    cargo_emit::rerun_if_changed!("{}", source_runner_crate_path.display());
 
-    let gitignore_path = source_runner_crate_dir.join(".gitignore");
+    let gitignore_path = source_runner_crate_path.join(".gitignore");
     let gitignore_file = gitignore::File::new(&gitignore_path).ok();
     let gitignore_filter = move |path: &Path| {
         !gitignore_file
@@ -32,26 +33,24 @@ pub fn main() {
             .unwrap_or(false)
     };
 
-    if !target_runner_crate_dir.exists() {
-        fs::create_dir_all(&target_runner_crate_dir).unwrap();
-        for entry in WalkDir::new(&source_runner_crate_dir)
-            .into_iter()
-            .filter_map(|e| e.ok())
-            .filter(|e| gitignore_filter(e.path()))
-        {
-            if entry.path_is_symlink() {
-                continue;
-            }
+    fs::create_dir_all(&target_runner_crate_dir).unwrap();
+    for entry in WalkDir::new(&source_runner_crate_path)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| gitignore_filter(e.path()))
+    {
+        if entry.path_is_symlink() {
+            continue;
+        }
 
-            let source_path = entry.path();
-            let relative_path = source_path.strip_prefix(&source_runner_crate_dir).unwrap();
-            let target_path = target_runner_crate_dir.join(relative_path);
-            if entry.file_type().is_dir() {
-                fs::create_dir(&target_path).unwrap();
-            } else {
-                assert!(entry.file_type().is_file());
-                fs::copy(&source_path, &target_path).unwrap();
-            }
+        let source_path = entry.path();
+        let relative_path = source_path.strip_prefix(&source_runner_crate_path).unwrap();
+        let target_path = target_runner_crate_path.join(relative_path);
+        if entry.file_type().is_dir() {
+            fs::create_dir(&target_path).unwrap();
+        } else {
+            assert!(entry.file_type().is_file());
+            fs::copy(&source_path, &target_path).unwrap();
         }
     }
     drop(gitignore_filter);
@@ -71,10 +70,11 @@ pub fn main() {
     if profile == Profile::Release {
         command.arg("--release");
     }
+
     command.spawn().unwrap().wait().unwrap().exit_ok().unwrap();
 
     let runner_executable_filename = format!("{}.exe", runner_crate_name);
-    let runner_executable_path = target_runner_crate_dir
+    let runner_executable_path = target_runner_crate_path
         .join("target")
         .join(&target_triple)
         .join(profile.as_str())
