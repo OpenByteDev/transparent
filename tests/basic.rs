b02998aa65d793f9958db74bb23f3102e5172108
OpenByte <development.openbyte@gmail.com>
2021-10-21 16:03:11 +0200

    Add test for correct exit code

--- tests/basic.rs
+++ tests/basic.rs
@@ -25,6 +25,30 @@ fn check_identical_output() {
         .unwrap();
     let opaque_output = cmd.spawn().unwrap().wait_with_output().unwrap();
 
+    assert_eq!(transparent_output.status.code(), opaque_output.status.code());
+    assert_eq!(transparent_output.stdout, opaque_output.stdout);
+    assert_eq!(transparent_output.stderr, opaque_output.stderr);
+}
+
+#[test]
+fn check_identical_non_zero_exit_code() {
+    let mut cmd = if cfg!(windows) {
+        let mut c = Command::new("powershell.exe");
+        c.arg("-Command").arg("&"); // Invalid command to make PS fail
+        c
+    } else {
+        Command::new("false")
+    };
+    cmd.stdout(Stdio::piped()).stderr(Stdio::piped());
+
+    let transparent_output = cmd
+        .spawn_transparent(&TransparentRunner::new())
+        .unwrap()
+        .wait_with_output()
+        .unwrap();
+    let opaque_output = cmd.spawn().unwrap().wait_with_output().unwrap();
+
+    assert!(!transparent_output.status.success());
     assert_eq!(transparent_output.status, opaque_output.status);
     assert_eq!(transparent_output.stdout, opaque_output.stdout);
     assert_eq!(transparent_output.stderr, opaque_output.stderr);
