05dda39eb25101b30850f06c5062574cf2774719
OpenByte <development.openbyte@gmail.com>
2021-10-21 16:02:35 +0200

    Fix example

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@ A crate for running processes on a virtual desktop / virtual X server environmen
 ## Usage
 This will spawn `some program` on a virtual desktop / virtual X server environment.
 ```rust
+# use std::process::*;
+# use transparent::*;
 Command::new("some program")
     .spawn_transparent(&TransparentRunner::new())
     .unwrap()
